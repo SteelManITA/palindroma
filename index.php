ea7f6395d4d140b8b1dc46832744a68d9f96ffc2
SteelManITA <concetto.pollicina@gmail.com>
2017-04-08 15:58:29 +0200

    aggiunto else non palindroma, righe 47-49

--- index.php
+++ index.php
@@ -44,6 +44,8 @@
 	<?php 
 		if (isset($isPalindroma) && $isPalindroma) {
 			echo "è palindroma";
+		} else {
+			echo "non è palindroma";
 		}
 	?>
 	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
